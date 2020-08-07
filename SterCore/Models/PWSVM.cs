a42cbfbdaef9afe8a9354e661dd1983276f9e134
Damian Lesik <damianlesik3@gmail.com>
2020-08-07 12:37:29 +0200

    added PWSVM

---
+++ SterCore/Models/PWSVM.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models
+{
+    public class PWSVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public DateTime DateFrom { get; set; }
+        [Required]
+        public DateTime DateTo { get; set; }
+        public string DestinationCountry { get; set; }
+        public string DestinationCity { get; set; }
+        public string PurposeOfTravel { get; set; }
+        public string TransportVehicle { get; set; }
+        public string AdditionalInfo { get; set; }
+        public int PrepaymentAmount { get; set; }
+        public string PrepaymentCurrency { get; set; }
+        public string ApplicationId { get; set; }
+        [Required]
+        public EmployeeVM Employee { get; set; }
+        public string EmployeeId { get; set; }
+
+        public IEnumerable<SelectListItem> Employees { get; set; }
+    }
+}

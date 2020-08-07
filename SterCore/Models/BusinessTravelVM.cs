b199e1eb384d906cc81e74fcb7c39fac2cf1ae64
Damian Lesik <damianlesik3@gmail.com>
2020-08-07 14:21:55 +0200

    fixed error notMapped

--- SterCore/Models/BusinessTravelVM.cs
+++ SterCore/Models/BusinessTravelVM.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -25,7 +26,7 @@ namespace leave_management.Models
         [Required]
         public EmployeeVM Employee { get; set; }
         public string EmployeeId { get; set; }
-
+        [NotMapped]
         public IEnumerable<SelectListItem> Employees { get; set; }
     }
 }

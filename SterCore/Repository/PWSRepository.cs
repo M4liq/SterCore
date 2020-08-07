3a5bda973a3844d732f2b8ee44892141ff0a2268
Damian Lesik <damianlesik3@gmail.com>
2020-08-07 12:01:03 +0200

    added first step of dependecy injection

--- SterCore/Repository/PWSRepository.cs
+++ SterCore/Repository/PWSRepository.cs
@@ -1,11 +1,53 @@
-﻿using System;
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace leave_management.Repository
 {
-    public class PWSRepository
+    public class PWSRepository : IPWSRepository
     {
+        private readonly ApplicationDbContext _db;
+
+        public PWSRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public Task<bool> Create(PWS entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(PWS entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Exists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<PWS>> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PWS> FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(PWS entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

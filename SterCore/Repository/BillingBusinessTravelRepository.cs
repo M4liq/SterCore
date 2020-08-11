f0354dcfd67376f240cc0e79a2576a5fd8852eb8
Damian Lesik <damianlesik3@gmail.com>
2020-08-11 12:24:01 +0200

    added interface and repository

---
+++ SterCore/Repository/BillingBusinessTravelRepository.cs
@@ -0,0 +1,47 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class BillingBusinessTravelRepository : IBillingBusinessTravelRepository
+    {
+        public Task<bool> Create(BillingBusinessTravel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(BillingBusinessTravel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Exists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<BillingBusinessTravel>> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BillingBusinessTravel> FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(BillingBusinessTravel entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

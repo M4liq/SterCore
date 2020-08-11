01a9e11a4a46399b557817ec03f7dd0a10893fff
Damian Lesik <damianlesik3@gmail.com>
2020-08-11 11:59:50 +0200

    added billing business travel data

---
+++ SterCore/Data/BillingBusinessTravel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Data
+{
+    public class BillingBusinessTravel
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+        public string EmployeeId { get; set; }
+        [ForeignKey("BusinessTravelId")]
+        public BusinessTravel BusinessTravel { get; set; }
+        public int BusinessTravelId { get; set; }
+        public int Amount { get; set; }
+        public bool IsPaidOut { get; set; }
+
+
+    }
+}

8ffe03a976f25ff289e5e8ddfc977b5b90545216
Damian Lesik <damianlesik3@gmail.com>
2020-08-19 19:46:05 +0200

    added MedicalCheckUpVM

---
+++ SterCore/Models/MedicalCheckUpVM.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models
+{
+    public class MedicalCheckUpVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public DateTime DateOfMedicalExamination { get; set; }
+        public DateTime ValidUntil { get; set; }
+        public string Comment { get; set; }
+        public bool isDisplayedToEmployee { get; set; }
+        public bool isDisplayedToSupervisor { get; set; }
+        public EmployeeVM Employee { get; set; }
+        [Required]
+        public string EmployeeId { get; set; }
+        [Required]
+        public int TypeOfMedicalCheckUpId { get; set; }
+        public IEnumerable<SelectListItem> Employees { get; set; }
+        public IEnumerable<SelectListItem> TypeOfMedicalCheckUps { get; set; }
+        public IEnumerable<SelectListItem> isDisplayedToEmployees { get; set; }
+        public IEnumerable<SelectListItem> isDisplayedToSupervisors { get; set; }
+
+    }
+}

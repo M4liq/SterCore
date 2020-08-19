1e9ba0e8b71b608cadc32eb274407e7e07977479
Damian Lesik <damianlesik3@gmail.com>
2020-08-19 18:50:09 +0200

    added interface and repository

---
+++ SterCore/Repository/MedicalCheckUpRepository.cs
@@ -0,0 +1,103 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using leave_management.Services.Components.ORI;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class MedicalCheckUpRepository : IMedicalCheckUpRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IOrganizationResourceManager _organizationManager;
+        public MedicalCheckUpRepository(ApplicationDbContext db, IOrganizationResourceManager organizationManager)
+        {
+            _db = db;
+            _organizationManager = organizationManager;
+        }
+        public async Task<bool> Create(MedicalCheckUp entity)
+        {
+            //ORI separating data beetween organizations
+            entity.OrganizationToken = _organizationManager.GetOrganizationToken();
+
+            await _db.MedicalCheckUps.AddAsync(entity);
+            return await Save();
+        }
+
+        public async Task<bool> Delete(MedicalCheckUp entity)
+        {
+            //ORI checking if data is from appropirate organization scope
+            if (entity.OrganizationToken != _organizationManager.GetOrganizationToken())
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            _db.MedicalCheckUps.Remove(entity);
+            return await Save();
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            var organizationToken = _organizationManager.GetOrganizationToken();
+
+            var exists = await _db.MedicalCheckUps
+                //ORI Filtring leave types by their tokens to get scope
+                .Where(q => q.OrganizationToken == organizationToken)
+                .AnyAsync(q => q.Id == id);
+            return exists;
+        }
+
+        public async Task<ICollection<MedicalCheckUp>> FindAll()
+        {
+            //ORI getting token to find organization scope
+            var organizationToken = _organizationManager.GetOrganizationToken();
+
+
+            var MedicalCheckUps = await _db.MedicalCheckUps
+                .Include(q => q.Employee)
+                .Include(q => q.typeOfMedicalCheckUp)
+                .Where(q => q.OrganizationToken == organizationToken)
+                .ToListAsync();
+            return MedicalCheckUps;
+        }
+
+        public async Task<MedicalCheckUp> FindById(int id)
+        {
+            //ORI getting token to find organization scope
+            var organizationToken = _organizationManager.GetOrganizationToken();
+
+            var MedicalCheckUp = await _db.MedicalCheckUps
+                //ORI Filtring organizations by their tokens to get scope
+                .Where(q => q.OrganizationToken == organizationToken)
+                .FirstOrDefaultAsync(q => q.Id == id);
+            return MedicalCheckUp;
+        }
+
+        public async Task<bool> Save()
+        {
+            var changes = await _db.SaveChangesAsync();
+            return changes > 0;
+        }
+
+        public void SetToken(MedicalCheckUp entity)
+        {
+            var token = _organizationManager.GetOrganizationToken();
+            entity.OrganizationToken = token;
+        }
+
+        public async Task<bool> Update(MedicalCheckUp entity)
+        {
+            //ORI checking if data is from appropirate organization scope
+            if (entity.OrganizationToken != _organizationManager.GetOrganizationToken())
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            _db.MedicalCheckUps.Update(entity);
+            return await Save();
+        }
+    }
+}

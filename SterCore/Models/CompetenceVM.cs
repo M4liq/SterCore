a067e511795a5215fead1d27961fdaf9bb068753
Damian Lesik <damianlesik3@gmail.com>
2020-08-21 16:43:32 +0200

    added VM for Competence and CompetenceType

---
+++ SterCore/Models/CompetenceVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models
+{
+    public class CompetenceVM
+    {
+        public int Id { get; set; }
+        public EmployeeVM Employee { get; set; }
+        public string EmployeeId { get; set; }
+        public CompetenceTypeVM CompetenceType { get; set; }
+        public int CompetenceTypeId { get; set; }
+        public DateTime DateValidUntil { get; set; }
+    }
+}

cc58287e83e2d0e09bd8c8fe7db8bf0168d78855
Damian Lesik <damianlesik3@gmail.com>
2020-08-21 17:06:09 +0200

    added competence index logic and view

---
+++ SterCore/Controllers/CompetenceController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using leave_management.Contracts;
+using leave_management.Data;
+using leave_management.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace leave_management.Controllers
+{
+    public class CompetenceController : Controller
+    {
+        private readonly ICompetenceRepository _competenceRepository;
+        private readonly ICompetenceTypeRepository _competenceTypeRepository;
+        private readonly IEmployeeRepository _employeeRepo;
+        private readonly UserManager<Employee> _userManager;
+        private readonly IMapper _mapper;
+        public CompetenceController(ICompetenceRepository competenceRepository, ICompetenceTypeRepository competenceTypeRepository, IEmployeeRepository employeeRepo, UserManager<Employee> userManager, IMapper mapper)
+        {
+            _competenceRepository = competenceRepository;
+            _competenceTypeRepository = competenceTypeRepository;
+            _employeeRepo = employeeRepo;
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+        // GET: Competence
+        public async Task<ActionResult> Index()
+        {
+            var competences = _competenceRepository.FindAll().Result;
+            var mappedModel = _mapper.Map<List<Competence>, List<CompetenceVM>>(competences.ToList());
+            var model = new List<CompetenceVM>();
+            var employees = await _userManager.GetUsersInRoleAsync("Employee");
+            foreach (var item in mappedModel)
+            {
+                model.Add(new CompetenceVM
+                {
+                   Id = item.Id,
+                   CompetenceTypeId = item.CompetenceTypeId,
+                   DateValidUntil = item.DateValidUntil,
+                   EmployeeFullName = employees.FirstOrDefault(q => q.Id == item.EmployeeId).Firstname.ToString() + " " + employees.FirstOrDefault(q => q.Id == item.EmployeeId).Lastname.ToString()
+                });
+            }
+            return View(model);
+        }
+
+        // GET: Competence/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Competence/Create
+        public async Task<ActionResult> Create()
+        {
+            return View();
+        }
+
+        // POST: Competence/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Competence/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Competence/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Competence/Delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Competence/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- SterCore/Models/CompetenceVM.cs
+++ SterCore/Models/CompetenceVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,8 +11,13 @@ namespace leave_management.Models
         public int Id { get; set; }
         public EmployeeVM Employee { get; set; }
         public string EmployeeId { get; set; }
+        [Display(Name = "Osoba")]
+        public string EmployeeFullName { get; set; }
         public CompetenceTypeVM CompetenceType { get; set; }
         public int CompetenceTypeId { get; set; }
+        [Display(Name = "Kompetencje")]
+        public string CompetenceName{ get; set; }
+        [Display(Name = "Ważne do")]
         public DateTime DateValidUntil { get; set; }
     }
 }

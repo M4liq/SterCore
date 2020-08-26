6f1626170add8e7c757471db3ead176fe142d3c2
Damian Lesik <damianlesik3@gmail.com>
2020-08-26 12:24:26 +0200

    added competence type edit logic and view

--- SterCore/Controllers/CompetenceTypeController.cs
+++ SterCore/Controllers/CompetenceTypeController.cs
@@ -84,22 +84,41 @@ namespace leave_management.Controllers
         // GET: CompetenceType/Edit/5
         public async Task<ActionResult> Edit(int id)
         {
-            return View();
+            var success = await _competenceTypeRepository.Exists(id);
+            if (!success)
+            {
+                return NotFound();
+            }
+            var competenceType = await _competenceTypeRepository.FindById(id);
+            var model = _mapper.Map<CompetenceTypeVM>(competenceType);
+            return View(model);
         }
 
         // POST: CompetenceType/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit(int id, IFormCollection collection)
+        public async Task<ActionResult> Edit(CompetenceTypeVM model)
         {
             try
             {
-                // TODO: Add update logic here
+                // TODO: Add insert logic here
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+                var competenceType = _mapper.Map<CompetenceType>(model);
+                var isSuccess = await _competenceTypeRepository.Update(competenceType);
+                if (!isSuccess)
+                {
+                    ModelState.AddModelError("", "Something went wrong");
+                    return View(model);
+                }
 
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
+                ModelState.AddModelError("", "Something went wrong");
                 return View();
             }
         }

5ca1bf9954e7d906ec49a4e7fc39bf4bf0da3efa
Damian Lesik <damianlesik3@gmail.com>
2020-08-26 15:29:31 +0200

    added notification data points

---
+++ SterCore/Data/Notification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using leave_management.Services.Components.ORI;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Data
+{
+    public class Notification :OrganizationResurceIdentifier
+    {
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+        public string EmployeeId { get; set; }
+        [ForeignKey("NotificationTypeId")]
+        public NotificationType NotificationType { get; set; }
+        public int NotificationTypeId { get; set; }
+        public DateTime DateOfNotification { get; set; }
+        public DateTime DateValidUntil { get; set; }
+        public string AdditionalInfo { get; set; }
+    }
+}
---
+++ SterCore/Data/NotificationType.cs
@@ -0,0 +1,14 @@
+﻿using leave_management.Services.Components.ORI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Data
+{
+    public class NotificationType : OrganizationResurceIdentifier
+    {
+        string name { get; set; }
+    }
+}

846fadf19a43c927251ce53824897e02e003aa26
Damian Lesik <damianlesik3@gmail.com>
2020-08-27 11:22:31 +0200

    added another missing inheritance

--- SterCore/Data/TypeOfBilling.cs
+++ SterCore/Data/TypeOfBilling.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using leave_management.Services.Components.ORI;
 
 namespace leave_management.Data
 {
-    public class TypeOfBilling
+    public class TypeOfBilling:OrganizationResurceIdentifier
     {
         public string Name { get; set; }
         public int Value { get; set; }

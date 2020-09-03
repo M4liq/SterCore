bad5537371e115905ed576413485dc41fa01d6d0
Damian Lesik <damianlesik3@gmail.com>
2020-09-03 12:57:13 +0200

    added data points for resource.cs

---
+++ SterCore/Data/Resource.cs
@@ -0,0 +1,27 @@
+﻿using leave_management.Services.Components.ORI;
+using leave_management.Services.ORI.Contracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+namespace leave_management.Data
+{
+    public class Resource
+    {
+        [Key]
+        public int Id { get; set; }
+        public string OrganizationToken { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+        public string EmployeeId { get; set; }
+        [ForeignKey("ResourceTypeId")]
+        public NotificationType ResourceType { get; set; }
+        public int ResourceTypeId { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateUntil { get; set; }
+        public string AdditionalInfo { get; set; }
+        public bool IsReturned { get; set; }
+    }
+}

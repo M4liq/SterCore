faa30723d5d1536604159f5a97eec87d38384503
Damian Lesik <damianlesik3@gmail.com>
2020-09-03 13:53:01 +0200

    added VMs

---
+++ SterCore/Models/ResourceTypeVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models
+{
+    public class ResourceTypeVM
+    {
+        public int Id { get; set; }
+        public string OrganizationToken { get; set; }
+        [Display(Name = "Nazwa")]
+        public string Name { get; set; }
+        [Display(Name = "Pokaż pole 'Data od'")]
+        public bool showDateFrom { get; set; }
+        [Display(Name = "Pokaż pole 'Data do'")]
+        public bool showDateUntil { get; set; }
+        [Display(Name = "Pokaż pole 'Zwrócono'")]
+        public bool showReturned { get; set; }
+    }
+}

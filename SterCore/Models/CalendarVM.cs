5c9479f78ae0be8a9941f0bbdf6eb9e351b177ef
Damian Lesik <damianlesik3@gmail.com>
2020-09-14 10:54:05 +0200

    added first version of calendar with many things to update

---
+++ SterCore/Models/CalendarVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models
+{
+    public class CalendarVM
+    {
+        public string ContractEmployeeFullName { get; set; }
+        public DateTime ContractDateOfContractAgreement { get; set; }
+        public DateTime ContractStartDate { get; set; }
+        public DateTime ContractEndDate { get; set; }
+
+        public string TrainingCourseEmployeeFullName { get; set; }
+
+        public DateTime TrainingCourseStartDate { get; set; }
+        public DateTime TrainingCourseEndDate { get; set; }
+
+        public string NotificationEmployeeFullName { get; set; }
+        public DateTime NotificationStartDate { get; set; }
+        public DateTime NotificationEndDate { get; set; }
+
+        public string MedicalCheckUpEmployeeFullName { get; set; }
+        
+        public DateTime MedicalCheckUpStartDate { get; set; }
+        public DateTime MedicalCheckUpEndDate { get; set; }
+    }
+}

afd55e18350e83f35a721e0793088ad45a2329ff
Damian Lesik <damianlesik3@gmail.com>
2020-09-15 10:34:19 +0200

    added contract and repo for schedule

---
+++ SterCore/Contracts/IWorkTimeScheduleRepository.cs
@@ -0,0 +1,13 @@
+﻿using leave_management.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Contracts
+{
+    public interface IWorkTimeScheduleRepository : IRepositoryBase<WorkTimeSchedule>
+    {
+    }
+}

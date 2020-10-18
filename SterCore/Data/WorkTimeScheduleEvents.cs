3e893b3d6d0ccc0bf0ce01f2ecf161786e02c562
Damian Lesik <damianlesik3@gmail.com>
2020-10-18 21:40:03 +0200

    added new Datapoints

---
+++ SterCore/Data/WorkTimeScheduleEvents.cs
@@ -0,0 +1,32 @@
+﻿using leave_management.Services.Components.ORI;
+using leave_management.Services.ORI.Contracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Data
+{
+    public class WorkTimeScheduleEvents : IApplicationDataView
+    {
+        [Key]
+        public int Id { get; set; }
+        public string OrganizationToken { get; set; }
+        public string DepartmentToken { get; set; }
+        public int EventId { get; set; }
+        public string Subject { get; set; }
+        public string Guid { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string ShiftStartTime { get; set; }
+        public string ShiftEndTime { get; set; }
+        public bool IsAllDay { get; set; }
+        public string EmployeeId { get; set; }
+        public string Description { get; set; }
+        public int PauseTimeLength { get; set; }
+        public string WorkTimeLength { get; set; }
+
+    }
+}
---
+++ SterCore/Repository/WorkTimeScheduleEventsRepository.cs
@@ -0,0 +1,77 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using leave_management.Services.Components.ORI;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class WorkTimeScheduleEventsRepository : IWorkTimeScheduleEventsRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IOrganizationResourceManager<WorkTimeScheduleEvents> _organizationManager;
+        public WorkTimeScheduleEventsRepository(ApplicationDbContext db, IOrganizationResourceManager<WorkTimeScheduleEvents> organizationManager)
+        {
+            _db = db;
+            _organizationManager = organizationManager;
+        }
+        public async Task<bool> Create(WorkTimeScheduleEvents entity)
+        {
+            _organizationManager.SetAccess(entity);
+            await _db.WorkTimeScheduleEvents.AddAsync(entity);
+            return await Save();
+        }
+
+        public async Task<bool> Delete(WorkTimeScheduleEvents entity)
+        {
+            if (!_organizationManager.VerifyAccess(entity))
+            {
+                throw new UnauthorizedAccessException();
+            }
+            _db.WorkTimeScheduleEvents.Remove(entity);
+            return await Save();
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            if (await FindById(id) == null)
+                return false;
+            else
+                return true;
+        }
+
+        public async Task<ICollection<WorkTimeScheduleEvents>> FindAll()
+        {
+            var WorkTimeScheduleEvents = _organizationManager.FilterDbSetByView(_db.WorkTimeScheduleEvents);
+            return await WorkTimeScheduleEvents.ToListAsync();
+        }
+
+        public async Task<WorkTimeScheduleEvents> FindById(int id)
+        {
+            var WorkTimeScheduleEvents = _organizationManager.FilterDbSetByView(_db.WorkTimeScheduleEvents);
+            return await WorkTimeScheduleEvents.FirstOrDefaultAsync(q => q.Id == id);
+        }
+
+        public async Task<bool> Save()
+        {
+            var changes = await _db.SaveChangesAsync();
+            return changes > 0;
+        }
+
+
+
+        public async Task<bool> Update(WorkTimeScheduleEvents entity)
+        {
+            //ORI checking if data is from appropirate organization scope
+            if (!_organizationManager.VerifyAccess(entity))
+            {
+                throw new UnauthorizedAccessException();
+            }
+            _db.WorkTimeScheduleEvents.Update(entity);
+            return await Save();
+        }
+    }
+}
